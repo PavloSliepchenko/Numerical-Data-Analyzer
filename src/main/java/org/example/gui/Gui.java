50cf35fbedbf28e1916e5c2431f862a5230a20c5
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2024-06-14 16:28:44 -0500

    Added Result object to introduce calculation results, added method to find the longest sequence, added methods to display the results

--- src/main/java/org/example/gui/Gui.java
+++ src/main/java/org/example/gui/Gui.java
@@ -3,14 +3,16 @@ package org.example.gui;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import java.util.List;
+import org.example.processor.Result;
 
 public class Gui {
     public String getFilePath() {
         String path = null;
         JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
         chooser.setAcceptAllFileFilterUsed(false);
-        FileNameExtensionFilter extFilter = new FileNameExtensionFilter("Text files"
-                , "txt");
+        FileNameExtensionFilter extFilter = new FileNameExtensionFilter("Text files", "txt");
         chooser.addChoosableFileFilter(extFilter);
         chooser.setDialogTitle("Choose a text file");
         int returnValue = chooser.showOpenDialog(null);
@@ -20,5 +22,34 @@ public class Gui {
             System.exit(0);
         }
         return path;
+    }
+
+    public void showResult(Result resultObject) {
+        List<String> result = resultObject.getResult();
+        printToConsole(resultObject);
+        String message = String.format("""
+                Max value: %s
+                Min value: %s
+                Median value: %s
+                Arithmetic mean value: %s
+                Longest increasing sequence: %s values
+                Longest decreasing sequence: %s values
+                """, result.get(0), result.get(1), result.get(2),
+                result.get(3), result.get(4), result.get(5));
+        JOptionPane.showMessageDialog(null, message, "Result", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    private void printToConsole(Result resultObject) {
+        List<String> result = resultObject.getResult();
+        System.out.println("Max value: " + result.get(0));
+        System.out.println("Min value: " + result.get(1));
+        System.out.println("Median value: " + result.get(2));
+        System.out.println("arithmeticMeanValue: " + result.get(3));
+        System.out.printf("Longest increasing sequence. Length: %s elements. Values:%n", result.get(4));
+        resultObject.getIncreaseSequence().forEach(System.out::println);
+        System.out.printf("Longest decreasing sequence. Length: %s elements. Values:%n", result.get(5));
+        resultObject.getDecreaseSequence().forEach(System.out::println);
+        System.out.printf("Time needed to process data (without file reading): %s seconds%n", result.get(6));
+        System.out.println("Number of elements in the file: " + result.get(7));
     }
 }
--- src/main/java/org/example/processor/DataProcessor.java
+++ src/main/java/org/example/processor/DataProcessor.java
@@ -1,30 +1,41 @@
 package org.example.processor;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
 public class DataProcessor {
-    public void process(List<Integer> fileData) {
-        Long beginning = new Date().getTime();
+    public Result process(List<Integer> fileData) {
+        List<String> result = new ArrayList<>();
+        long start = new Date().getTime();
         List<Integer> copy = new ArrayList<>(fileData);
         copy.sort(Comparator.naturalOrder());
         Integer max = copy.get(copy.size() - 1);
+        result.add(max.toString());
         Integer min = copy.get(0);
-        Double median = getMedian(copy);
-        Long sum = 0L;
+        result.add(min.toString());
+        double median = getMedian(copy);
+        result.add(Double.toString(median));
+        long sum = 0L;
         for (Integer element : fileData) {
             sum += element;
         }
-        Long end = new Date().getTime();
-        System.out.println("Max value: " + max);
-        System.out.println("Min value: " + min);
-        System.out.println("Median value: " + median);
-        System.out.println("File numbers sum: " + sum);
-        System.out.println("File size: " + fileData.size());
-        System.out.println("Time needed to calculate: " + (end - beginning) / 1000.0 + "s");
-
+        double arithmeticMeanValue = sum / (fileData.size() * 1.0);
+        result.add(Double.toString(arithmeticMeanValue));
+        long end = new Date().getTime();
+        List<Integer> increaseSequence = longestSequence(fileData);
+        result.add(String.valueOf(increaseSequence.size()));
+        copy = new ArrayList<>(fileData);
+        Collections.reverse(copy);
+        List<Integer> decreaseSequence = longestSequence(copy);
+        Collections.reverse(decreaseSequence);
+        result.add(String.valueOf(decreaseSequence.size()));
+        double timeNeededToCalculate = (end - start) / 1000.0;
+        result.add(Double.toString(timeNeededToCalculate));
+        result.add(String.valueOf(fileData.size()));
+        return new Result(result, increaseSequence, decreaseSequence);
     }
 
     private Double getMedian(List<Integer> fileData) {
@@ -32,5 +43,43 @@ public class DataProcessor {
             return (fileData.get(fileData.size() / 2 - 1) + fileData.get(fileData.size() / 2)) / 2.0;
         }
         return fileData.get(fileData.size() / 2) * 1.0;
+    }
+
+    private List<Integer> longestSequence(List<Integer> fileData) {
+        if (fileData.isEmpty()) {
+            return new ArrayList<>();
+        }
+        int from = 0;
+        int to = 0;
+        int count = 0;
+
+        int currentFrom = 0;
+        int currentTo = 0;
+        int currentCount = 0;
+        for (int i = 1; i < fileData.size(); i++) {
+            if (currentCount == 0 && fileData.get(i) > fileData.get(i - 1)) {
+                currentCount = 2;
+                currentFrom = i - 1;
+                currentTo = i + 1;
+            } else if (fileData.get(i) > fileData.get(i - 1)) {
+                currentTo = i + 1;
+                currentCount++;
+            } else {
+                if (currentCount > count) {
+                    count = currentCount;
+                    from = currentFrom;
+                    to = currentTo;
+                }
+                currentCount = 0;
+                currentFrom = 0;
+                currentTo = 0;
+            }
+        }
+        if (currentCount > count) {
+            count = currentCount;
+            from = currentFrom;
+            to = currentTo;
+        }
+        return count == 0 ? new ArrayList<>() : fileData.subList(from, to);
     }
 }
